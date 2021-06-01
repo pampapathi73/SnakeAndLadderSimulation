46f095ee66c93b48364048b0652027b5bb38b249
pampapathi73 <pampapathik32@gmail.com>
2021-06-01 18:38:47 +0530

    [pampapathi] Added : player Rolls A Die

--- SnakesAndLadderGame/SnakeLadder.cs
+++ SnakesAndLadderGame/SnakeLadder.cs
@@ -10,6 +10,15 @@ namespace SnakesAndLadderGame
         {
             int position = 0;
             Console.WriteLine("Player is at position : " + position);
+            Random random = new Random();
+            int droll = 0;
+            while (true)
+            {
+                droll = random.Next(6);
+                if (droll != 0) break;
+
+            }
+            Console.WriteLine(droll);
         }
     }
 }
