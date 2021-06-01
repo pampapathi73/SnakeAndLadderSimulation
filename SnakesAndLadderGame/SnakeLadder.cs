9b2b4ca4cb377d07d9703687483a982c7a72c2d4
pampapathi73 <pampapathik32@gmail.com>
2021-06-01 19:21:18 +0530

    [pampapathi] Added : dice play to win count

--- SnakesAndLadderGame/SnakeLadder.cs
+++ SnakesAndLadderGame/SnakeLadder.cs
@@ -13,58 +13,77 @@ namespace SnakesAndLadderGame
         public static int Position_check = 0;
         public void snake()
         {
-            int position = 0;
-            Console.WriteLine("Player is at position : " + position);
-            Random random = new Random();
-            int droll = 0;
-            while (true)
-            {
-                droll = random.Next(6);
-                if (droll != 0) break;
+			Console.WriteLine("Welcome to SnakeAndLadder program");
+			int noOfDieRoll = 0;
+			int position = 0;
 
-            }
-            Console.WriteLine(droll);
-            Random ran = new Random();
-            int option = 0;
-            while (true)
-            {
-                option = ran.Next(4);
-                if (option != 0) break;
-            }
-            Console.WriteLine("====OPTIONS======\nOption 1 : No Play\nOption 2 : Ladder \nOption 3 : Snake \n");
-            Console.WriteLine("Option : " + option);
+			Console.WriteLine("Start position of player :	" + position);
 
-            switch (option)
-            {
-                case 1:
-                    Console.WriteLine("Player stays in the same position : " + position);
-                    break;
+			while (position < Win_position)
+			{
 
-                case 2:
-                    if ((position + droll) > Win_position)
-                    {
-                        position = Win_position;
-                    }
-                    else
-                    {
-                        position += droll;
-                    }
-                    Console.WriteLine("Player moves ahead by : " + position);
-                    break;
+				Random random = new Random();
+				int droll = 0;
+				while (true)
+				{
+					droll = random.Next(7);
+					if (droll != 0) break;
+				}
 
-                case 3:
-                    position -= droll;
-                    if ((position - droll) < Position_check)
-                    {
-                        position = Position_check;
-                    }
-                    Console.WriteLine("Player moves behind by : " + position);
-                    break;
-                default:
-                    Console.WriteLine("Enter correct value");
-                    break;
+				Console.WriteLine("Number of dice : " + droll);
 
-            }
+				Random ran = new Random();
+				noOfDieRoll++;
+				int option = 0;
+				while (true)
+				{
+					option = ran.Next(4);
+					if (option != 0) break;
+				}
+
+				Console.WriteLine("====OPTIONS======\nOption 1 : No Play\nOption 2 : Ladder \nOption 3 : Snake \n");
+
+				Console.WriteLine("Option : " + option);
+				//Switch statement
+				switch (option)
+				{
+					case 1:
+
+						Console.WriteLine("Player stays in the same position : " + position);
+						break;
+
+					case 2:
+						if ((position + droll) > Win_position)
+						{
+							position = Win_position;
+						}
+						else
+						{
+							position += droll;
+						}
+
+						Console.WriteLine("Player moves ahead by : " + position);
+						break;
+
+					case 3:
+						position -= droll;
+						if ((position - droll) < Position_check)
+						{
+							position = Position_check;
+						}
+
+						Console.WriteLine("Player moves behind by : " + position);
+						break;
+
+					default:
+
+						Console.WriteLine("Enter correct value");
+						break;
+				}
+			}
+
+			Console.WriteLine("Number Of Times The Dice Was Played To Win The Game Is : " + noOfDieRoll);
+
+		}
         }
     }
-}
